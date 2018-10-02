c45798c1b30e723234c95cb74f2d0309618722b4
juancki <jcgp97@gmail.com>
2018-10-02 10:49:56 +0200

    main.c formated beatifully

--- main.c
+++ main.c
@@ -1,104 +1,98 @@
-#include <stdio.h>
-#include <ncurses.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include "conway.h"
+#include <ncurses.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 // CONSTANTS
-const int block = ' '|A_REVERSE;
-const long period = 1000*500; // MICROS
+const int block = ' ' | A_REVERSE;
+const long period = 1000 * 500; // MICROS
 // Main variables
 int WIDTH;
 int HEIGHT;
 WINDOW *win;
-chtype ** field;
+chtype **field;
 
 /**
  * WORKS
  */
-chtype** createField(int sqy, int sqx){
-	chtype ** f = (chtype **) malloc(sizeof(chtype *)*sqy);
-	for (int j = 0 ; j<sqy;  j++){
-		f[j] = (chtype *) malloc(sizeof(chtype)*sqx);
-		// Init to spaces
-	}
-	for (int y=0; y < sqy; y++)
-	{
-		for (int x=0; x < sqx; x++)
-			f[y][x] = ' ';
-	}
-	return f;
+chtype **createField(int sqy, int sqx) {
+  chtype **f = (chtype **)malloc(sizeof(chtype *) * sqy);
+  for (int j = 0; j < sqy; j++) {
+    f[j] = (chtype *)malloc(sizeof(chtype) * sqx);
+    // Init to spaces
+  }
+  for (int y = 0; y < sqy; y++) {
+    for (int x = 0; x < sqx; x++)
+      f[y][x] = ' ';
+  }
+  return f;
 }
 /**
  * WORKS
  */
-void printFieldToStdout()
-{
-	for(int j = 0 ; j < HEIGHT; j++){
-		// printf("%d:\t%s \n",i,(char*) field[i]);
-		printf("%d:\t",j);
-		for (int i = 0 ;i<WIDTH; i++){
-			printf("%d ",(int)field[j][i]-' ');
-		}
-		printf("\n");
-	}
+void printFieldToStdout() {
+  for (int j = 0; j < HEIGHT; j++) {
+    // printf("%d:\t%s \n",i,(char*) field[i]);
+    printf("%d:\t", j);
+    for (int i = 0; i < WIDTH; i++) {
+      printf("%d ", (int)field[j][i] - ' ');
+    }
+    printf("\n");
+  }
 }
-
-
 
 /**
  * Fill the field global variable of values printed on the screen.
  * TODO
  */
-void readSubwindow(WINDOW* w){
-	for (int i = 0; i <HEIGHT ; i++)
-		winchstr(w,field[i]);
+void readSubwindow(WINDOW *w) {
+  for (int i = 0; i < HEIGHT; i++)
+    winchstr(w, field[i]);
 }
 /**
  * Fills the window with the values of the field
  */
-void printToSubwindow(WINDOW* w)
-{
-	wmove(w,0,0);
-	wrefresh(win);
-	for(int j = 0 ;j < HEIGHT ; j++)
-	{
-		for(int i = 0; i < WIDTH; i++){
-			mvwaddch(w,j,i,(char) field[j][i]);
-		}
-	}
-	wrefresh(win);
+void printToSubwindow(WINDOW *w) {
+  wmove(w, 0, 0);
+  wrefresh(win);
+  for (int j = 0; j < HEIGHT; j++) {
+    for (int i = 0; i < WIDTH; i++) {
+      mvwaddch(w, j, i, (char)field[j][i]);
+    }
+  }
+  wrefresh(win);
 }
 /**
  * Draws the border of the field game.
  * + for vertexes
  * | or - for edges
  */
-void drawSquare(){
-	int xx, yy;
-	getmaxyx(stdscr,yy,xx);
+void drawSquare() {
+  int xx, yy;
+  getmaxyx(stdscr, yy, xx);
 
-	WIDTH = xx-2;
-	HEIGHT = yy-3;
-	printw("W :%d, H :%d",WIDTH,HEIGHT);
-	int xpadding=0;
-	int ypadding=1;
-	int topx = xpadding+WIDTH+1;
-	int topy = ypadding+HEIGHT+1;
-	// Drawing top-bottom ends
-	mvaddch(ypadding,xpadding,'+');
-	mvaddch(topy,xpadding,'+');
-	mvaddch(ypadding,topx,'+');
-	mvaddch(topy,topx,'+');
-	// Drawing borders
-	for(int x=xpadding+1; x < topx; x++){
-		mvaddch(ypadding,x,'-');
-		mvaddch(topy,x,'-');
-	}
-	for(int y=ypadding+1; y < topy; y++){
-		mvaddch(y,xpadding,'|');
-		mvaddch(y,topx,'|');
-	}
+  WIDTH = xx - 2;
+  HEIGHT = yy - 3;
+  printw("W :%d, H :%d", WIDTH, HEIGHT);
+  int xpadding = 0;
+  int ypadding = 1;
+  int topx = xpadding + WIDTH + 1;
+  int topy = ypadding + HEIGHT + 1;
+  // Drawing top-bottom ends
+  mvaddch(ypadding, xpadding, '+');
+  mvaddch(topy, xpadding, '+');
+  mvaddch(ypadding, topx, '+');
+  mvaddch(topy, topx, '+');
+  // Drawing borders
+  for (int x = xpadding + 1; x < topx; x++) {
+    mvaddch(ypadding, x, '-');
+    mvaddch(topy, x, '-');
+  }
+  for (int y = ypadding + 1; y < topy; y++) {
+    mvaddch(y, xpadding, '|');
+    mvaddch(y, topx, '|');
+  }
 }
 
 /**
@@ -106,10 +100,10 @@ void drawSquare(){
  * Prints header
  * Prints field border
  */
-void resetWindow(){
-	erase();
-	printw("Conway's Game of Live");	/* Print Hello World		  */
-	drawSquare();
+void resetWindow() {
+  erase();
+  printw("Conway's Game of Live"); /* Print Hello World		  */
+  drawSquare();
 }
 
 /**
@@ -117,51 +111,41 @@ void resetWindow(){
  * and sets to blank spaces
  */
 
-void startGame()
-{
+void startGame() {
   initall();
   resetWindow();
-  refresh();			/* Print it on to the real screen */
-  field = createField(HEIGHT,WIDTH);
+  refresh(); /* Print it on to the real screen */
+  field = createField(HEIGHT, WIDTH);
 }
 
-
-
-
-int main()
-{
+int main() {
   startGame();
-  win =subwin(stdscr, HEIGHT, WIDTH, 2, 1);
-  do{
-	resetWindow();
-  	mvwaddch(win,0,0,block);
-  	mvwaddch(win,5,0,block);
-	wprintw(win,"aqui");
+  win = subwin(stdscr, HEIGHT, WIDTH, 2, 1);
+  do {
+    resetWindow();
+    mvwaddch(win, 0, 0, block);
+    mvwaddch(win, 5, 0, block);
+    wprintw(win, "aqui");
 
-  	mvwaddch(win,5,5,block);
-  	mvwaddch(win,15,0,block);
-	wrefresh(win);
-	refresh();
-	usleep(1000);
-  }while(false);
+    mvwaddch(win, 5, 5, block);
+    mvwaddch(win, 15, 0, block);
+    wrefresh(win);
+    refresh();
+    usleep(1000);
+  } while (false);
 
-  refresh();			/* Print it on to the real screen */
-  //readSubwindow(win);
-
-
-  getch();			/* Wait for user input */
+  refresh(); /* Print it on to the real screen */
+  // readSubwindow(win);
+  getch(); /* Wait for user input */
   printToSubwindow(win);
-  mvwprintw(win,4,4,"NEW");
-  mvprintw(4,2,"QSA");
+  mvwprintw(win, 4, 4, "NEW");
+  mvprintw(4, 2, "QSA");
   wrefresh(win);
   refresh();
   getch();
   enditall();
-//  readSubwindow(win);
-//  printFieldToStdout();
+  //  readSubwindow(win);
+  //  printFieldToStdout();
 }
-
-
-
 
 /*************************************************/
